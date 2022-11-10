20c1453f3ed191f4c5a3f7285030eac34ec5936b
Ji ZHANG <hope.zh@gmail.com>
2022-11-10 17:49:34 +0800

    code clean up

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import "./bWLwgP.css";
 import Plotly from "plotly.js-dist-min";
 
-// new code ====================================================================
 // chart 1 ---------------------------------------------------------------------
 var trace1_1 = {
 	x: [1, 2, 3, 4],
@@ -125,28 +124,3 @@ function update() {
 }
 
 requestAnimationFrame(update);
-
-// old code ====================================================================
-// import "./style.css";
-// import javascriptLogo from "../static/javascript.svg";
-// import { setupCounter } from "./counter.js";
-
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vitejs.dev" target="_blank">
-//       <img src="../static/vite.svg" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
