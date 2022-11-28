5fcc384ebccc88a54bf9fbf99e0fa9805cc82b8e
Ji <hope.zh@gmail.com>
2022-11-28 17:31:26 +0700

    use d3.csv()

--- src/main_d3.js
+++ src/main_d3.js
@@ -1,29 +1,44 @@
 import "./bWLwgP.css";
 import * as d3 from "d3";
 
-// console.log(d3);
-
-// fetch text
-const fetchText = async (url) => {
-	const response = await fetch(url);
-	return await response.text();
-};
-
+// get csv file ----------------------------------------------------------------
 const csvURL =
 	"https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv";
 
 const appleCSV = "../static/data/apple.csv";
 
-const csvTextPromise = fetchText(csvURL);
+// option 1: use fetch text async func and d3.csvParse(text) -------------------
+// const fetchText = async (url) => {
+// 	const response = await fetch(url);
+// 	return await response.text();
+// };
 
-csvTextPromise.then((text) => {
-	const data = d3.csvParse(text); // parse csv as an array by row
+// const csvTextPromise = fetchText(csvURL);
 
-    let message = ''
-    message += 'text file size: ' + Math.round(text.length / 1024) + 'kB' + '\n'
-    message += "num of rows: " + data.length + '\n'
-    message += 'num of columns: ' + data.columns.length + '\n'
+// csvTextPromise.then((text) => {
+// 	const data = d3.csvParse(text); // parse csv as an array by row
 
-    document.getElementById('message').textContent = message
-	// console.log("column names:", data.columns);
+// 	let message = "";
+// 	message +=
+// 		"text file size: \t" + Math.round(text.length / 1024) + "kB" + "\n";
+// 	message += "num of rows in csv: \t" + data.length + "\n";
+// 	message += "num of columns: \t" + data.columns.length + "\n";
+// 	// message += "column names: \t" + data.columns + '\n'
+
+// 	document.getElementById("message").textContent = message;
+// });
+
+// option 2: use d3.csv()
+d3.csv(csvURL).then((data) => {
+	let message = "";
+	message +=
+		"text file size: \t" +
+		Math.round(d3.csvFormat(data).length / 1024) +
+		"kB" +
+		"\n";
+	message += "num of rows in csv: \t" + data.length + "\n";
+	message += "num of columns: \t" + data.columns.length + "\n";
+	message += "column names: \t" + data.columns + "\n";
+
+	document.getElementById("message").textContent = message;
 });
