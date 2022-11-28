96097f4ea2960298d038d85a8389aced95670649
Ji <hope.zh@gmail.com>
2022-11-28 20:03:41 +0700

    use d3 to read csv, use plotly to add line chart, finally...

--- src/main_d3.js
+++ src/main_d3.js
@@ -1,5 +1,6 @@
 import "./bWLwgP.css";
 import * as d3 from "d3";
+import Plotly from "plotly.js-dist-min";
 
 // get csv file ----------------------------------------------------------------
 const csvURL =
@@ -28,17 +29,48 @@ const appleCSV = "../static/data/apple.csv";
 // 	document.getElementById("message").textContent = message;
 // });
 
-// option 2: use d3.csv()
-d3.csv(csvURL).then((data) => {
-	let message = "";
-	message +=
-		"text file size: \t" +
-		Math.round(d3.csvFormat(data).length / 1024) +
-		"kB" +
-		"\n";
-	message += "num of rows in csv: \t" + data.length + "\n";
-	message += "num of columns: \t" + data.columns.length + "\n";
-	message += "column names: \t" + data.columns + "\n";
+// option 2: use d3.csv() ------------------------------------------------------
+// d3.csv(csvURL).then((data) => {
+// 	let message = "";
+// 	message +=
+// 		"text file size: \t" +
+// 		Math.round(d3.csvFormat(data).length / 1024) +
+// 		"kB" +
+// 		"\n";
+// 	message += "num of rows in csv: \t" + data.length + "\n";
+// 	message += "num of columns: \t" + data.columns.length + "\n";
+// 	message += "column names: \t" + data.columns + "\n";
 
-	document.getElementById("message").textContent = message;
+// 	document.getElementById("message").textContent = message;
+// });
+
+// func to create line chart ---------------------------------------------------
+const addLineChart = (_x, _y, _div) => {
+	var trace = {
+		x: _x,
+		y: _y,
+		mode: "lines",
+		type: "scatter",
+	};
+
+	var data = [trace];
+
+	Plotly.newPlot(_div, data);
+};
+
+// get data by column name -----------------------------------------------------
+const data2 = d3.csv(csvURL);
+
+let date = [];
+let AAPL_Open = [];
+
+data2.then((data) => {
+	date = data.map((row) => row["Date"]);
+	AAPL_Open = data.map((row) => row["AAPL.Open"]);
+
+	console.log(date);
+	console.log(AAPL_Open);
+
+    addLineChart(date, AAPL_Open, 'plotly-g00');
 });
+
